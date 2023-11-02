86fdced9c8eadf6e0694b00eaab9f3def5a98398
Brenton Fletcher <i@bloople.net>
2023-11-02 19:12:52 +1100

    By default, share the Public folder instead of the entire C:\ drive, which should be slightly safer ;). Make default configuration file self documenting. Automatically generate a suitable password by default, so that the user can easily enable credentialed access.

--- Servel.NET/SourceGenerationContexts.cs
+++ Servel.NET/SourceGenerationContexts.cs
@@ -3,16 +3,19 @@
 namespace Servel.NET;
 
 [JsonSourceGenerationOptions(
+    GenerationMode = JsonSourceGenerationMode.Serialization,
     PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
-    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
-    GenerationMode = JsonSourceGenerationMode.Serialization)]
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault)]
 [JsonSerializable(typeof(DirectoryEntry))]
 [JsonSerializable(typeof(IEnumerable<string>))]
 internal partial class SerializationSourceGenerationContext : JsonSerializerContext
 {
 }
 
-[JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Metadata)]
+[JsonSourceGenerationOptions(
+    GenerationMode = JsonSourceGenerationMode.Metadata,
+    ReadCommentHandling = System.Text.Json.JsonCommentHandling.Skip,
+    AllowTrailingCommas = true)]
 [JsonSerializable(typeof(ServelOptions))]
 internal partial class ServelOptionsSourceGenerationContext : JsonSerializerContext
 {
