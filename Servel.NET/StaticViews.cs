42342f4d173caaddb39d6349b587787eca91d836
Brenton Fletcher <i@bloople.net>
2023-11-15 22:56:58 +1100

    Load HTML views from source directory in DEBUG mode to allow easy development of the HTML views

--- Servel.NET/StaticViews.cs
+++ Servel.NET/StaticViews.cs
@@ -6,6 +6,20 @@ public static class StaticViews
 {
     public static string Get(string name)
     {
+#if DEBUG
+        return GetDebug(name);
+#else
+        return GetRelease(name);
+#endif
+    }
+
+    private static string GetDebug(string name)
+    {
+        return File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", name));
+    }
+
+    private static string GetRelease(string name)
+    {
         using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"Servel.NET.Views.{name}");
         using var reader = new StreamReader(stream!);
         return reader.ReadToEnd();
