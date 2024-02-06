eade88e72292300d4be621b827a5b1eba5dbe140
Brenton Fletcher <i@bloople.net>
2024-02-06 21:18:55 +1100

    If user presses back button when gallery is open, return back to the listing

--- Servel.NET/Assets/js/index.js
+++ Servel.NET/Assets/js/index.js
@@ -2,16 +2,32 @@
 
 var Index = (function() {
   function jumpListing() {
+    window.location.hash = "";
+  }
+
+  function jumpGallery() {
+    window.location.hash = "#gallery";
+  }
+
+  function openListing() {
     Gallery.hide();
     Listing.show();
   }
 
-  function jumpGallery() {
+  function openGallery() {
     Listing.hide();
     Gallery.show();
   }
 
+  function onHashChange() {
+    const hash = window.location.hash.substring(1);
+    if(hash == "gallery") openGallery();
+    else if(hash == "") openListing();
+  }
+
   async function init() {
+    window.addEventListener("hashchange", onHashChange);
+
     const response = await fetch(window.location.href, {
       headers: {
         "Accept": "application/json",
@@ -25,7 +41,7 @@ var Index = (function() {
     Listing.init();
     Gallery.init();
     Entries.update();
-    jumpListing();
+    onHashChange();
   }
 
   return {
