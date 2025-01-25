ac19bb2c1a8cfc1792875aab4113ddf3a64cbcb5
Brenton Fletcher <i@bloople.net>
2025-01-25 13:17:08 +1100

    Why do I do this to myself?

--- Servel.NET/FileProviders/ListingFileInfo.cs
+++ Servel.NET/FileProviders/ListingFileInfo.cs
@@ -7,39 +7,39 @@ namespace Servel.NET.FileProviders;
 // Based on https://github.com/dotnet/runtime/blob/c8acea22626efab11c13778c028975acdc34678f/src/libraries/Microsoft.Extensions.FileProviders.Physical/src/PhysicalFileInfo.cs
 public class ListingFileInfo : IFileInfo
 {
-    private readonly PhysicalFileInfo wrapped;
-    private readonly FileInfo info;
-    private readonly FileInfo resolvedInfo;
+    private readonly PhysicalFileInfo _physicalFileInfo;
+    private readonly FileInfo _info;
+    private readonly FileInfo _resolvedInfo;
 
     public ListingFileInfo(PhysicalFileInfo physicalFileInfo)
     {
-        wrapped = physicalFileInfo;
-        info = GetInfoField(wrapped);
+        _physicalFileInfo = physicalFileInfo;
+        _info = GetInfoField(_physicalFileInfo);
 
         try
         {
-            var targetInfo = info.ResolveLinkTarget(true) as FileInfo;
-            resolvedInfo = targetInfo ?? info;
+            var targetInfo = _info.ResolveLinkTarget(true) as FileInfo;
+            _resolvedInfo = targetInfo ?? _info;
         }
         catch(FileNotFoundException)
         {
-            resolvedInfo = info;
+            _resolvedInfo = _info;
         }
     }
 
-    public bool Exists => resolvedInfo.Exists;
+    public bool Exists => _resolvedInfo.Exists;
 
-    public long Length => resolvedInfo.Length;
+    public long Length => _resolvedInfo.Length;
 
-    public string PhysicalPath => info.FullName;
+    public string PhysicalPath => _info.FullName;
 
-    public string Name => info.Name;
+    public string Name => _info.Name;
 
-    public DateTimeOffset LastModified => resolvedInfo.LastWriteTimeUtc;
+    public DateTimeOffset LastModified => _resolvedInfo.LastWriteTimeUtc;
 
     public bool IsDirectory => false;
 
-    public Stream CreateReadStream() => wrapped.CreateReadStream();
+    public Stream CreateReadStream() => _physicalFileInfo.CreateReadStream();
 
     [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_info")]
     private extern static ref FileInfo GetInfoField(PhysicalFileInfo @this);
